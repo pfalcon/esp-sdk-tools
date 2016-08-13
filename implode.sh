5e841e81385d6fb606db09ab43f915c68501a782
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-08-13 12:43:50 +0300

    implode.sh: Turn into single script, merging explode.sh.
    Also, link relocatable object and executable independently, each from
    individual objects. This allows to create a useful map file.

--- implode.sh
+++ implode.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 #
-# This script takes a single object file as produced by "explode.sh"
-# tool and links it into self-contained executable file.
+# This scripts extracts object files from Espressif SDK libs, and
+# links all objects together into a single relocatable object, and
+# into a seld-contained executable file.
 #
 
 set -e
@@ -11,23 +12,58 @@ if [ "$1" == "" ]; then
     exit 1
 fi
 
-xtensa-lx106-elf-gcc -c empty_user.c
-xtensa-lx106-elf-gcc -c empty_libgcc.c
+if [ ! -d "$1" ]; then
+    echo "Create $1/ subdir, and put into it all lib/lib*a from that SDK version, together with ld/eagle.app.v6.ld"
+    exit 1
+fi
+
+cd $1
+
+TOP=$PWD
+echo $TOP
+to_link=""
+
+# Remove superfluous/duplicate libs
+rm -f liblwip_536.a libupgrade.a libat.a libjson.a libdriver.a libgcc.a
+
+for f in *.a; do
+    dir=$(basename $f .a)
+    echo $f $dir
+    mkdir -p $dir
+    cd $dir
+    ar x ../$f
+    to_link="$to_link $dir/*.o"
+    cd $TOP
+done
+
+# This references ap_get_sta, which may be not defined in some (all?) SDK versions
+rm -f libwpa/ieee802_1x.o
+# Duplicate of wpa2/eap_common.o
+rm -f libwps/eap_common.o
+# Duplicate of wpa2/bignum.o
+rm -f libcrypto/bignum.o
+
+xtensa-lx106-elf-ld $to_link -r -o esp8266-sdk-$1.o
+
+# Former implode.sh
+
+xtensa-lx106-elf-gcc -c ../empty_user.c
+xtensa-lx106-elf-gcc -c ../empty_libgcc.c
 
 # Different SDK versions use different memory layouts, so we should
 # use per-version linker script.
-LDSCRIPT=$1/eagle.app.v6.ld
+LDSCRIPT=eagle.app.v6.ld
 
 xtensa-lx106-elf-ld \
 --emit-relocs \
 --no-check-sections \
 -nostdlib \
--Map=$1/esp8266-sdk-$1.map --cref \
--L$1 \
+-Map=esp8266-sdk-$1.map --cref \
+-L. \
 -T $LDSCRIPT \
--T region-override.ld \
-$1/esp8266-sdk-$1.o empty_user.o empty_libgcc.o \
--o $1/esp8266-sdk-$1 \
+-T ../region-override.ld \
+$to_link ../empty_user.o ../empty_libgcc.o \
+-o esp8266-sdk-$1 \
 $(xtensa-lx106-elf-gcc -print-file-name=libc.a) \
 
 # No longer link with libgcc to avoid iRAM segment overflow,
